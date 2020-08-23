ea69eec3fac345d42ffb8fdcc0ee6b3a13c47b38
Joanne Lee <jokang1005@gmail.com>
2020-08-23 16:48:51 -0500

    tried to set up ajax and click event on submit button, having troublt with .replace and hyperlinks do not appear for ajax

--- studio_ghibli.app/app.js
+++ studio_ghibli.app/app.js
@@ -1,1 +1,23 @@
 console.log ('hi world')
+
+$('form').on('submit', (event) => {
+    event.preventDefault()
+    const $input = $('input[type="text"]')
+    console.log ($($input).val().replace(/" "/g, "+"))
+
+
+    $.ajax({
+        url: `https://ghibliapi.herokuapp.com/films/?title=My+Neighbor+Totoro`
+    }).then (
+        (data) => {
+            console.log (data);
+            $movieCharacters = $('<div>').html(data[0].people)
+            $('body').append($movieCharacters)
+            
+        },
+        () => {
+            console.log ('bad request')
+        }
+        )
+});
+
