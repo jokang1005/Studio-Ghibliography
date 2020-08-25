73af247a33a02854bb062c4ccff4faba6fca6b1f
Joanne Lee <jokang1005@gmail.com>
2020-08-24 22:40:06 -0500

    first ajax

--- studio_ghibli.app/app.js
+++ studio_ghibli.app/app.js
@@ -1,23 +1,41 @@
-console.log ('hi world')
+// console.log ('hi world')
+// const $princessMononokeButton = $('#Princess+Mononoke')
+// $($princessMononokeButton).on('click', (event) => {
+//     event.preventDefault()
+//     // const $input = $('input[type="text"]')
+//     // const $parseInt = $($input).val().replace(/" "/g, "+")
+//     // console.log ($parseInt)
+//     console.log ("button was clicked")
 
-$('form').on('submit', (event) => {
-    event.preventDefault()
-    const $input = $('input[type="text"]')
-    console.log ($($input).val().replace(/" "/g, "+"))
 
-
-    $.ajax({
-        url: `https://ghibliapi.herokuapp.com/films/?title=My+Neighbor+Totoro`
-    }).then (
-        (data) => {
-            console.log (data);
-            $movieCharacters = $('<div>').html(data[0].people)
-            $('body').append($movieCharacters)
+//     // $.ajax({
+//     //     url: `https://ghibliapi.herokuapp.com/films/?title=Princess+Mononoke`
+//     // }).then (
+//     //     (data) => {
+//     //         console.log (data);
+//     //         $movieCharacters = $('<div>').html(data[0].people)
+//     //         $('body').append($movieCharacters)
             
-        },
-        () => {
-            console.log ('bad request')
-        }
-        )
+//     //     },
+//     //     () => {
+//     //         console.log ('bad request')
+//     //     }
+//     //     )
+    
+
+// });
+
+const $button = $('<button id="Princess+Mononoke">').text("Princess Mononoke")
+$('body').append($button)
+$($button).on('click', (event) => {
+    console.log ($($button).text())
+
 });
 
+$.ajax({
+    url: `https://ghibliapi.herokuapp.com/films/?title=${$($button).text()}`
+}).then (
+    (data) => {
+        console.log (data);
+    }
+)
