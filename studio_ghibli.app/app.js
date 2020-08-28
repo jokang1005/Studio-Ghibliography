4e6a66c1a98e35c606c568ee74198e508ac1d523
Joanne Lee <jokang1005@gmail.com>
2020-08-28 13:05:03 -0500

    carousel working now, try to make more DRY

--- studio_ghibli.app/app.js
+++ studio_ghibli.app/app.js
@@ -1,51 +1,195 @@
-let num = 0;
+////////////////////////////PHOTO CACHE/////////////////////////////
 const $princessMononokePhoto = $('<img src="https://upload.wikimedia.org/wikipedia/en/8/8c/Princess_Mononoke_Japanese_poster.png"/>')
-$(window).on("load",()=> {
-$.ajax({
-    url: `https://ghibliapi.herokuapp.com/films/?title=Princess+Mononoke`
-}).then (
-    (data) => {
-        $('#photo').append($princessMononokePhoto)
-        $('#title').html(`Title: ${data[0].title}`)
-        $('#description').html(`${data[0].description}`)
-        $('#director').html(`Director: ${data[0].director}`)
-        $('#producer').html(`Producer: ${data[0].producer}`)
-        $('#rt_score').html(`Rating: ${data[0].rt_score}`)
-    }
-)
-console.log ("window loaded")
-})
+const $castleInTheSkyPhoto = $('<img src="https://www.reelviews.net/resources/img/posters/thumbs/castle_poster.jpg"/>')
+const $graveOfTheFirefliesPhoto = $('<img src="https://m.media-amazon.com/images/M/MV5BOWNjMTU2OWQtMGYzMi00MWQzLWI1MzItMDRkMjU2MzNkNTI2XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>').width("265px")
+const $myNeighborTotoroPhoto = $('<img src="https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg"/>')
+const $kikisDeliveryServicePhoto = $('<img src="https://assets0.dostuffmedia.com/uploads/aws_asset/aws_asset/3234733/7fe351ee-2c2e-469d-8dda-99f096ca9086.jpg"/>').width("265px")
+const $onlyYesterdayPhoto = $('<img src="https://upload.wikimedia.org/wikipedia/en/4/46/OYpost.jpg"/>')
+const $porcoRossoPhoto = $('<img src="https://images-na.ssl-images-amazon.com/images/I/818IhaeGLYL._SY445_.jpg"/>')
+const $pomPokoPhoto = $('<img src="https://m.media-amazon.com/images/M/MV5BODFjMjMyNjktNzc1ZS00NzMwLWJhMDgtNDk0OWNhNTkxNjMzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg"/>')
+const $whisperOfTheHeartPhoto = $('<img src="https://m.media-amazon.com/images/M/MV5BZDg0MWNmNjktMGEwZC00ZDlmLWI1MTUtMDBmNjQzMWM2NjBjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>').width("265px")
+const $myNeighborsTheYamadasPhoto = $('<img src="https://target.scene7.com/is/image/Target/GUEST_a7c82256-5823-4fca-9734-55d02e83d2e8?wid=488&hei=488&fmt=pjpeg"/>')
+const $spiritedAwayPHoto = $('<img src="https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>').width("265px")
+const $theCatReturnsPhoto = $('<img src="https://prodimage.images-bn.com/pimages/0826663181593_p0_v1_s550x406.jpg"/>')
+const $howlsMovingCastlePhoto = $('<img src="https://m.media-amazon.com/images/M/MV5BNmM4YTFmMmItMGE3Yy00MmRkLTlmZGEtMzZlOTQzYjk3MzA2XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>').width("265px")
+const $talesFromEarthseaPhoto = $('<img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Gedo6sn.jpg/220px-Gedo6sn.jpg"/>').width("265px")
+const $ponyoPhoto = $('<img src="https://prodimage.images-bn.com/pimages/0826663181654_p0_v2_s1200x630.jpg"/>').width("265px")
+const $arriettyPhoto = $('<img src="https://upload.wikimedia.org/wikipedia/en/e/e7/Karigurashi_no_Arrietty_poster.png"/>')
+const $fromUpOnPoppyHillPhoto = $('<img src="https://images-na.ssl-images-amazon.com/images/I/817VldXYslL._SY445_.jpg"/>')
+const $theWindRisesPhoto = $('<img src="https://m.media-amazon.com/images/M/MV5BMTU4NDg0MzkzNV5BMl5BanBnXkFtZTgwODA3Mzc1MDE@._V1_.jpg"/>').width("265px")
+const $theTaleOfThePrincessKaguyaPhoto = $('<img src="https://m.media-amazon.com/images/M/MV5BNThmMWMyMWMtOWRiNy00MGY0LTg1OTUtNjYzODg2MjdlZGU5XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>')
+const $whenMarnieWasTherePhoto = $('<img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/When_Marnie_Was_There.png/220px-When_Marnie_Was_There.png"/>')
 
-$('form').on('submit', (event) => {
-    event.preventDefault();
-    // $('#container').empty()    
-    let $text = $('input[type="text"]').val()
-    // console.log ($text)
-    if ($text === "castle in the sky") {
-        console.log ("it worked")
-    }
+/////////////////////PRINCESS MONONOKE CHARACTERS////////////////////
+const $ashitakaPhoto = $('<img src="https://i.pinimg.com/originals/88/27/af/8827af5f693b19f0010b8adafa94a301.jpg"/>')
+const $sanPhoto = $('<img src="https://www.bluefinbrands.com/media/wysiwyg/brand-landing-pages/princess_mononoke.jpg"/>')
+const $eboshiPhoto = $('<img src="https://i.pinimg.com/originals/0c/1f/55/0c1f5599943c2690755c684108f73b7e.jpg"/>')
+const $jigoPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/7/72/Jigo.jpg/revision/latest/top-crop/width/220/height/220?cb=20181011142443"/>')
+const $kohrokuPhoto = $('<img src="https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/thumbs/kohroku-princess-mononoke-5_thumb.jpg"/>')
+const $gonzaPhoto = $('<img src="https://www.animecharactersdatabase.com/uploads/chars/thumbs/200/5688-2082147945.jpg"/>')
+const $hiisamaPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/8/83/Hii-sama.jpg/revision/latest?cb=20181011141901"/>')
+const $yakulPhoto = $('<img src="https://i.pinimg.com/originals/b2/c7/e6/b2c7e6dbc50fc08c7f0ca60933f622ff.gif"/>')
+const $shishigamiPhoto = $('<img src="https://i.pinimg.com/originals/ba/4b/3b/ba4b3b0bbefd46fafc54f250ee82bcfe.jpg"/>')
+const $moroPhoto = $('<img src="https://i.pinimg.com/originals/db/e3/bc/dbe3bc3aa213049afeac0d60d1f72f29.jpg"/>')
+
+///////////////////////////PONYO CHARACTERS//////////////////////////
+const $sosukePhoto = $('<img src="https://vignette.wikia.nocookie.net/p__/images/8/86/Sosuke_n%C3%A4rbilden.png/revision/latest?cb=20130227090701&path-prefix=protagonist"/>')
+
+//POSSIBLE CONST FOR DRY//
+const myFunc = () => {}
+    $('.character').empty()
+        for (const item in data) {
+        let $name = $(`<div id=${data[item].name} class="abc">`)
+        $($name).append(`<div>${data[item].name}</div>
+        <div>${data[item].gender}</div>
+        <div>${data[item].age}</div>`)
+        $('.character').append($name)
+        }
+
+/////////////////////ON LOAD AJAX///////////////////////////////////
+$(window).on("load",()=> {
     $.ajax({
-        url: `https://ghibliapi.herokuapp.com/films/?title=${$text}`
+        url: `https://ghibliapi.herokuapp.com/films/?title=Princess+Mononoke`
     }).then (
         (data) => {
-    //         // $('#photo').html($princessMononokePhoto)
-            
+            $('#photo').append($princessMononokePhoto)
             $('#title').html(`Title: ${data[0].title}`)
             $('#description').html(`${data[0].description}`)
             $('#director').html(`Director: ${data[0].director}`)
             $('#producer').html(`Producer: ${data[0].producer}`)
             $('#rt_score').html(`Rating: ${data[0].rt_score}`)
-            //I want to make a carousel of the pictures of characters that are clickable and takes you to the people ajax.
-            // $('#characters').html(data[0].people)
+              
+        }
+    )
+///////////////////ON LOAD CAROUSEL///////////////////
 
-            console.log (data[0].title)
-    //         })
+    $.ajax({
+        url:`https://ghibliapi.herokuapp.com/people/?name=Ashitaka&name=San&name=Yakul&name=Shishigami&name=Moro&name=Eboshi&name=Jigo&name=Kohroku&name=Gonza&name=Hii-sama`
+    }).then (
+        (data) => {
             
-    //     },
-    //     () => {
-    //         console.log ('bad request')
-        })
+            for (const item in data) {
+            let $name = $(`<div id=${data[item].name} class="abc">`)
+            // $($name).append($ashitakaPhoto)
+            $($name).append(`<div>${data[item].name}</div>
+            <div>${data[item].gender}</div>
+            <div>${data[item].age}</div>`)
+            $('.character').append($name)
+            }
+        $('#Ashitaka').prepend($ashitakaPhoto)
+        $('#San').prepend($sanPhoto)
+        $('#Eboshi').prepend($eboshiPhoto)
+        $('#Jigo').prepend($jigoPhoto)
+        $('#Kohroku').prepend($kohrokuPhoto)
+        $('#Gonza').prepend($gonzaPhoto)
+        $('#Hii-sama').prepend($hiisamaPhoto)
+        $('#Yakul').prepend($yakulPhoto)
+        $('#Shishigami').prepend($shishigamiPhoto)
+        $('#Moro').prepend($moroPhoto)
+        }
+    )
+    console.log ("window loaded")
+    })
 
-//     $.ajax({
-//         url: 
+////////////////////ON CLICK CAROUSEL///////////////////////
+
+let currentIndex = 0;
+let highestIndex = $('.abc').lengtßh - 1;
+$('#previous-button').on('click', () => {
+    console.log ("CLICK!")
+    
+})
+
+$('#next-button').on('click',() => {
+    console.log ("CLICK CLICK!")
+
+    $('.abc').eq(currentIndex).css('display', 'none')
+
+    if (currentIndex < highestIndex) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    $('.abc').eq(currentIndex).css('display', 'block')
+})
+
+
+
+//////////////////////////////ON SUBMIT/////////////////////////////
+    $('form').on('submit', (event) => {
+        event.preventDefault();
+        $('#photo').empty()    
+        let $text = $('input[type="text"]').val()
+        // console.log ($text)
+        if ($text.toLowerCase() === "castle in the sky") {
+
+            $('#photo').append($castleInTheSkyPhoto)
+        } else if ($text.toLowerCase() === "grave of the fireflies") {
+            $('#photo').append($graveOfTheFirefliesPhoto)
+        } else if ($text.toLowerCase() === "my neighbor totoro") {
+            $('#photo').append($myNeighborTotoroPhoto)
+        } else if ($text.toLowerCase() === "kiki's delivery service") {
+            $('#photo').append($kikisDeliveryServicePhoto)
+        } else if ($text.toLowerCase() === "only yesterday") {
+            $('#photo').append($onlyYesterdayPhoto)
+        } else if ($text.toLowerCase() === "porco rosso") {
+            $('#photo').append($porcoRossoPhoto)
+        } else if ($text.toLowerCase() === "pom poko") {
+            $('#photo').append($pomPokoPhoto)
+        } else if ($text.toLowerCase() === "whisper of the heart") {
+            $('#photo').append($whisperOfTheHeartPhoto)
+        } else if ($text.toLowerCase() === "my neighbors the yamadas") {
+            $('#photo').append($myNeighborsTheYamadasPhoto)
+        } else if ($text.toLowerCase() === "spirited away") {
+            $('#photo').append($spiritedAwayPHoto)
+        } else if ($text.toLowerCase() === "the cat returns") {
+            $('#photo').append($theCatReturnsPhoto)
+        } else if ($text.toLowerCase() === "howl's moving castle") {
+            $('#photo').append($howlsMovingCastlePhoto)
+        } else if ($text.toLowerCase() === "tales from earthsea") {
+            $('#photo').append($talesFromEarthseaPhoto)
+        } else if ($text.toLowerCase() === "ponyo") {
+            $('#photo').append($ponyoPhoto)
+        } else if ($text.toLowerCase() === "arrietty") {
+            $('#photo').append($arriettyPhoto)
+        } else if ($text.toLowerCase() === "from up on poppy hill") {
+            $('#photo').append($fromUpOnPoppyHillPhoto)
+        } else if ($text.toLowerCase() === "the wind rises") {
+            $('#photo').append($theWindRisesPhoto)
+        } else if ($text.toLowerCase() === "the tale of the princess kaguya") {
+            $('#photo').append($theTaleOfThePrincessKaguyaPhoto)
+        } else if ($text.toLowerCase() === "when marnie was there") {
+            $('#photo').append($whenMarnieWasTherePhoto)
+        }
+        $.ajax({
+            url: `https://ghibliapi.herokuapp.com/films/?title=${$text}`
+        }).then (
+            (data) => {
+                
+                $('#title').html(`Title: ${data[0].title}`)
+                $('#description').html(`${data[0].description}`)
+                $('#director').html(`Director: ${data[0].director}`)
+                $('#producer').html(`Producer: ${data[0].producer}`)
+                $('#rt_score').html(`Rating: ${data[0].rt_score}`)
+            })
+        
+        /////////////////////ON SUBMIT CAROUSEL//////////////////////
+        if ($text.toLowerCase() === "ponyo") {
+            $.ajax({
+                url: `https://ghibliapi.herokuapp.com/people`
+            }).then (
+                (data) => {
+                    $('.character').empty()
+                    for (const item in data) {
+                    let $name = $(`<div id=${data[item].name} class="abc">`)
+                    $($name).append(`<div>${data[item].name}</div>
+                    <div>${data[item].gender}</div>
+                    <div>${data[item].age}</div>`)
+                    $('.character').append($name)
+                    }
+                $('#Sosuke').prepend($sosukePhoto)
+                
+                })
+        }
+        
     })
