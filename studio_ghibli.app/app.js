13e9f465c8ce5b180e0af3cf89b81870bedf7af8
Joanne Lee <jokang1005@gmail.com>
2020-08-28 20:23:12 -0500

    made arrays with photos for more general code

--- studio_ghibli.app/app.js
+++ studio_ghibli.app/app.js
@@ -22,33 +22,61 @@ const $theWindRisesPhoto = $('<img src="https://m.media-amazon.com/images/M/MV5B
 const $theTaleOfThePrincessKaguyaPhoto = $('<img src="https://m.media-amazon.com/images/M/MV5BNThmMWMyMWMtOWRiNy00MGY0LTg1OTUtNjYzODg2MjdlZGU5XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>')
 const $whenMarnieWasTherePhoto = $('<img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/When_Marnie_Was_There.png/220px-When_Marnie_Was_There.png"/>')
 
-/////////////////////PRINCESS MONONOKE CHARACTERS////////////////////
-const $ashitakaPhoto = $('<img src="https://i.pinimg.com/originals/88/27/af/8827af5f693b19f0010b8adafa94a301.jpg"/>')
-const $sanPhoto = $('<img src="https://www.bluefinbrands.com/media/wysiwyg/brand-landing-pages/princess_mononoke.jpg"/>')
-const $eboshiPhoto = $('<img src="https://i.pinimg.com/originals/0c/1f/55/0c1f5599943c2690755c684108f73b7e.jpg"/>')
-const $jigoPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/7/72/Jigo.jpg/revision/latest/top-crop/width/220/height/220?cb=20181011142443"/>')
-const $kohrokuPhoto = $('<img src="https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/thumbs/kohroku-princess-mononoke-5_thumb.jpg"/>')
-const $gonzaPhoto = $('<img src="https://www.animecharactersdatabase.com/uploads/chars/thumbs/200/5688-2082147945.jpg"/>')
-const $hiisamaPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/8/83/Hii-sama.jpg/revision/latest?cb=20181011141901"/>')
-const $yakulPhoto = $('<img src="https://i.pinimg.com/originals/b2/c7/e6/b2c7e6dbc50fc08c7f0ca60933f622ff.gif"/>')
-const $shishigamiPhoto = $('<img src="https://i.pinimg.com/originals/ba/4b/3b/ba4b3b0bbefd46fafc54f250ee82bcfe.jpg"/>')
-const $moroPhoto = $('<img src="https://i.pinimg.com/originals/db/e3/bc/dbe3bc3aa213049afeac0d60d1f72f29.jpg"/>')
-
-///////////////////////////PONYO CHARACTERS//////////////////////////
-const $sosukePhoto = $('<img src="https://vignette.wikia.nocookie.net/p__/images/8/86/Sosuke_n%C3%A4rbilden.png/revision/latest?cb=20130227090701&path-prefix=protagonist"/>')
-
-/////////////////////PORCO ROSSO CHARACTERS//////////////////////////
-const $porcoRossoPhoto2 = $('<img src="https://cdn1.i-scmp.com/sites/default/files/styles/1200x800/public/images/methode/2016/08/04/f76c586a-546a-11e6-98ca-49b1c9e3ed10_1280x720.JPG?itok=TlS8Ale6"/>')
+const arr = [
 
 ///////////////////////////CASTLE IN THE SKY/////////////////////////
-const $lusheetaPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/5/58/Castleinthesky-disneyscreencaps.com-1682.jpg/revision/latest/top-crop/width/360/height/450?cb=20200517192457"/>')
-const $dolaPhoto = $('<img src="https://vignette.wikia.nocookie.net/disney/images/c/c3/5688-2094236141.jpg/revision/latest?cb=20140915192650"/>')
-const $romskaPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/d/d5/Muska.jpg/revision/latest?cb=20181031023656"/>')
-const $unclePomPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/d/de/Uncle_Pom.png/revision/latest?cb=20180809131657"/>')
-const generalMuoroPhoto = $('<img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1547370964i/26909839.jpg"/>')
-// const duffiPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/0/0a/Duffi.png/revision/latest?cb=2017050418304"/>')
-const charlesPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/2/28/Charlies.jpg/revision/latest/scale-to-width-down/325?cb=20181031025246"/>')
-const louisPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/8/80/Louis.jpg/revision/latest?cb=20181031025734"/>')
+    $('<img src="https://lh3.googleusercontent.com/proxy/IADMrx7Tljkyu2ZcObQUJzwi5z9zxjtSFTj162SAbx7nZVNEVYN2Yx_3jC-Frsv4v15cMfL5Txm_vMqZzXV_5opBwcF59PtkCDIfjTCSvoG3EKYdDLcro7IcVh89tZ-d37pl6Juoh1s"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/5/58/Castleinthesky-disneyscreencaps.com-1682.jpg/revision/latest/top-crop/width/360/height/450?cb=20200517192457"/>'),
+    $('<img src="https://vignette.wikia.nocookie.net/disney/images/c/c3/5688-2094236141.jpg/revision/latest/top-crop/width/360/height/450?cb=20140915192650"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/d/d5/Muska.jpg/revision/latest/top-crop/width/360/height/450?cb=20181031023656"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/thumbs/uncle-pom-19051.jpg?t=1584048841"/>'),
+    $('<img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1547370964i/26909839.jpg"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/0/0a/Duffi.png/revision/latest?cb=20170504183048"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/8/80/Louis.jpg/revision/latest?cb=20181031025734"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/2/28/Charlies.jpg/revision/latest/scale-to-width-down/325?cb=20181031025246"/>'),
+    $('<img src="https://lh3.googleusercontent.com/proxy/x-BrVmQSM_e2FWM27vRg0YDsQHc2-wTJLfBq-vlitst2_vXwE36ikQwuDBfQlnaqL0nooFEGIKlRrCJcdZN8h_oljUxvy-EM6VOA2cNymSCbb1necQNgHlKLku0M7lPIZ6D-dT59wg9Qs3JSx0nS"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/1/17/Eggman_laputa.jpg/revision/latest?cb=20181031021525"/>'),
+    $('<img src="https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/thumbs/okami-duffi-laputa-the-castle-in-the-sky-4.48_thumb.jpg"/>'),
+/////////////////////PRINCESS MONONOKE CHARACTERS////////////////////
+    $('<img src="https://i.pinimg.com/originals/88/27/af/8827af5f693b19f0010b8adafa94a301.jpg"/>'),
+    $('<img src="https://www.bluefinbrands.com/media/wysiwyg/brand-landing-pages/princess_mononoke.jpg"/>'),
+    $('<img src="https://i.pinimg.com/originals/0c/1f/55/0c1f5599943c2690755c684108f73b7e.jpg"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/7/72/Jigo.jpg/revision/latest/top-crop/width/220/height/220?cb=20181011142443"/>'),
+    $('<img src="https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/thumbs/kohroku-princess-mononoke-5_thumb.jpg"/>'),
+    $('<img src="https://www.animecharactersdatabase.com/uploads/chars/thumbs/200/5688-2082147945.jpg"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/8/83/Hii-sama.jpg/revision/latest?cb=20181011141901"/>'),
+    $('<img src="https://i.pinimg.com/originals/b2/c7/e6/b2c7e6dbc50fc08c7f0ca60933f622ff.gif"/>'),
+    $('<img src="https://i.pinimg.com/originals/ba/4b/3b/ba4b3b0bbefd46fafc54f250ee82bcfe.jpg"/>'),
+    $('<img src="https://i.pinimg.com/originals/db/e3/bc/dbe3bc3aa213049afeac0d60d1f72f29.jpg"/>'),
+/////////////////////KIKI'S DELIVERY SERVICE/////////////////////////
+    $('<img src="https://i.pinimg.com/originals/33/25/d9/3325d90043e779964a97c6f668b51950.jpg"/>'),
+///////////////////////MY NEIGHBOR TOTORO////////////////////////////
+    $('<img src="https://www.anime-planet.com/images/characters/satsuki-kusokabe-2138.jpg?t=1559033412"/>'),
+    $('<img src="https://cdn-us.anidb.net/images/main/29227.jpg"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/tatsuo-kusakabe-2142.jpg"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/yasuko-kusakabe-2143.jpg"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/granny-my-neighbor-totoro-3678.jpg?t=1559034846"/>'),
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/c/c3/Kanta.jpg/revision/latest/top-crop/width/360/height/450?cb=20181028000058"/>'),
+    $('<img src="https://i.pinimg.com/originals/59/9b/0d/599b0db468f1806c855ee6506e2ae157.gif"/>'),
+    $('<img src="https://ctl.s6img.com/society6/img/aW85AdufMP-Rt83uiNeZrB55DDE/w_700/prints/~artwork/s6-0013/a/4079133_11288751/~~/chu--chibi-totoro-pop-art-white-version-prints.jpg"/>'),
+    $('<img src="https://i.pinimg.com/originals/a5/92/2e/a5922e26290a5610c5c9a65929654907.jpg"/>'),
+    $('<img src="https://vignette.wikia.nocookie.net/myneighbortotoro/images/3/30/Catbus.jpg/revision/latest/scale-to-width-down/340?cb=20140206190410"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/niya-26598.jpg?t=1564949743"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/muta-2248.jpg"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/the-cat-king-2250.jpg"/>'),
+    $('<img src="https://i.pinimg.com/originals/42/5f/d5/425fd52f672ffb65d2e0e5800c941c8d.png"/>'),
+    $('<img src="https://vignette.wikia.nocookie.net/p__/images/2/27/Haru_Yoshioka.jpg/revision/latest/top-crop/width/360/height/450?cb=20200104081742&path-prefix=protagonist"/>'),
+    $('<img src="https://www.anime-planet.com/images/characters/baron-humbert-von-gikkingen-2247.jpg?t=1588709286"/>'),
+    $('<img src="https://shortcut-test2.s3.amazonaws.com/uploads/role/attachment/65085/default_natori_2252.jpg"/>'),
+///////////////////////////CASTLE IN THE SKY/////////////////////////
+    $('<img src="https://static.wikia.nocookie.net/studio-ghibli/images/d/d5/Muska.jpg/revision/latest/top-crop/width/360/height/450?cb=20181031023656"/>'),
+/////////////////////PORCO ROSSO CHARACTERS//////////////////////////
+
+    $('<img src="https://cdn1.i-scmp.com/sites/default/files/styles/1200x800/public/images/methode/2016/08/04/f76c586a-546a-11e6-98ca-49b1c9e3ed10_1280x720.JPG?itok=TlS8Ale6"/>'),
+///////////////////////////PONYO CHARACTERS//////////////////////////
+
+    $('<img src="https://vignette.wikia.nocookie.net/p__/images/8/86/Sosuke_n%C3%A4rbilden.png/revision/latest?cb=20130227090701&path-prefix=protagonist"/>')
+]
 
 
 //POSSIBLE CONST FOR DRY//
@@ -65,6 +93,8 @@ const louisPhoto = $('<img src="https://static.wikia.nocookie.net/studio-ghibli/
 
 /////////////////////ON LOAD AJAX///////////////////////////////////
 $(window).on("load",()=> {
+        num = [12,13,14,15,16,17,18,19,20,21,22]
+
     $.ajax({
         url: `https://ghibliapi.herokuapp.com/films/?title=Princess+Mononoke`
     }).then (
@@ -81,28 +111,18 @@ $(window).on("load",()=> {
 ///////////////////ON LOAD CAROUSEL///////////////////
 
     $.ajax({
-        url:`https://ghibliapi.herokuapp.com/people/?name=Ashitaka&name=San&name=Yakul&name=Shishigami&name=Moro&name=Eboshi&name=Jigo&name=Kohroku&name=Gonza&name=Hii-sama`
+        url:`https://ghibliapi.herokuapp.com/people/`
     }).then (
         (data) => {
-            
-            for (const item in data) {
-            let $name = $(`<div id=${data[item].name} class="abc">`)
-            // $($name).append($ashitakaPhoto)
-            $($name).append(`<div>${data[item].name}</div>
-            <div>${data[item].gender}</div>
-            <div>${data[item].age}</div>`)
+            // $('.character').empty()
+            for (let i = 0; i < num.length; i++) {
+            let $name = $(`<div id=${data[num[i]].name} class="abc">`)
+            $($name).append(`<div>${data[num[i]].name}</div>
+            <div>${data[num[i]].gender}</div>
+            <div>${data[num[i]].age}</div>`)
+            $($name).prepend(arr[num[i]])
             $('.character').append($name)
             }
-        $('#Ashitaka').prepend($ashitakaPhoto)
-        $('#San').prepend($sanPhoto)
-        $('#Eboshi').prepend($eboshiPhoto)
-        $('#Jigo').prepend($jigoPhoto)
-        $('#Kohroku').prepend($kohrokuPhoto)
-        $('#Gonza').prepend($gonzaPhoto)
-        $('#Hii-sama').prepend($hiisamaPhoto)
-        $('#Yakul').prepend($yakulPhoto)
-        $('#Shishigami').prepend($shishigamiPhoto)
-        $('#Moro').prepend($moroPhoto)
         }
     )
     console.log ("window loaded")
@@ -123,7 +143,6 @@ $('#previous-button').on('click', () => {
     }
     $('.abc').eq(currentIndex).css('display', 'block')
 })
-// let highest = $('.abc').length-1
 let currentIndex = 0;
 
 $('#next-button').on('click',() => {
@@ -146,9 +165,8 @@ $('#next-button').on('click',() => {
         event.preventDefault();
         $('#photo').empty()    
         let $text = $('input[type="text"]').val()
-        // console.log ($text)
+        // switch this to switch statement if time allots
         if ($text.toLowerCase() === "castle in the sky") {
-
             $('#photo').append($castleInTheSkyPhoto)
         } else if ($text.toLowerCase() === "grave of the fireflies") {
             $('#photo').append($graveOfTheFirefliesPhoto)
@@ -205,7 +223,7 @@ $('#next-button').on('click',() => {
         } else if ($text.toLowerCase() === "porco rosso") {
             num = [41]
         } else if ($text.toLowerCase() === "castle in the sky") {
-            num = [1,2,3,4,5,6,7,8,9,10,11,40]
+            num = [0,1,2,3,4,5,6,7,8,9,10,11,40]
         } else if ($text.toLowerCase() === "the cat returns") {
             num = [34,35,36,37,38,39]
         } else if ($text.toLowerCase() === "whisper of the heart") {
@@ -216,7 +234,11 @@ $('#next-button').on('click',() => {
             num = [23,24,25,26,27,28,29,30,31,32]
         } else if ($text.toLowerCase() === "princess mononoke") {
             num = [12,13,14,15,16,17,18,19,20,21,22]
-        } 
+        } else {
+            num = []
+        }
+
+        
          $.ajax({
                 url: `https://ghibliapi.herokuapp.com/people/`
             }).then (
@@ -227,12 +249,25 @@ $('#next-button').on('click',() => {
                     $($name).append(`<div>${data[num[i]].name}</div>
                     <div>${data[num[i]].gender}</div>
                     <div>${data[num[i]].age}</div>`)
+                    $($name).prepend(arr[num[i]])
                     $('.character').append($name)
                     }
-                $('#Sosuke').prepend($sosukePhoto)
-                $('#Porco').prepend($porcoRossoPhoto2)
-                $('#Lusheeta').prepend($lusheetaPhoto)
+                // $('#Sosuke').prepend($sosukePhoto)
+                // $('#Porco').prepend($porcoRossoPhoto2)
+                // $('#Lusheeta').prepend($lusheetaPhoto)
                 
+                // $('#Romska').prepend($romskaPhoto)
+                // $('#Uncle').prepend($unclePomPhoto)
+                // $('#General').prepend($generalMuoroPhoto)
+                // $('#Duffi').prepend($duffiPhoto)
+                // $('#Louis').prepend($louisPhoto)
+                // $('#Charles').prepend($charlesPhoto)
+                // $('#Henri').prepend($henryPhoto)
+                // $('#Motro').prepend($motroPhoto)
+                // $('#Okami').prepend($okamiPhoto)
+                // $('#Colonel').prepend($colonelMuskaPhoto)
+
+
                 
                 })
             // $.ajax({
